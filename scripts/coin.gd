fe3713fefd1008bd5ec55bd8a385009f63fc574f
dev-joaovitor <joaotorvitor1@gmail.com>
2025-03-13 13:34:12 -0300

    feat: collect `coin` under development

---
+++ scripts/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+		print("+1 coin")
+		$CoinSprite.set_animation("collected")
+
+func _process(delta: float) -> void:
+	if $CoinSprite.get_animation() == "collected":
+		if not $CoinSprite.is_playing():
+			queue_free()
